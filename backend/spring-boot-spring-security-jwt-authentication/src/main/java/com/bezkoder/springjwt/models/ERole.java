6e7e861962da924163eb43bba623a792d6fd5e13
Yingrui Zhang <yingruizhang@yingruiz-macbook-pro.local>
2023-03-08 12:54:46 +1100

    added backend again

---
+++ backend/spring-boot-spring-security-jwt-authentication/src/main/java/com/bezkoder/springjwt/models/ERole.java
@@ -0,0 +1,11 @@
+package com.bezkoder.springjwt.models;
+
+public enum ERole {
+  ROLE_USER,
+  ROLE_MODERATOR,
+  ROLE_ADMIN,
+  ROLE_RECRUITER,
+  ROLE_INTERVIEWER,
+  ROLE_AC_COORDINATOR,
+  ROLE_SUPER_RECRUITER
+}
---
+++ backend/spring-boot-spring-security-jwt-authentication/src/main/java/com/bezkoder/springjwt/repository/InterviewsRepository.java
@@ -0,0 +1,14 @@
+package com.bezkoder.springjwt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bezkoder.springjwt.models.Interview;
+
+
+@Repository
+public interface InterviewsRepository extends JpaRepository<Interview, Integer> {
+
+	
+	
+}
