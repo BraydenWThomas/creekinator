9614791ff6248a3eabe058a4197dfab1fe4b9670
Yingrui Zhang <yingruizhang@yingruiz-macbook-pro.local>
2023-03-08 12:50:49 +1100

    added backend

---
+++ backend/spring-boot-spring-security-jwt-authentication/src/main/java/com/bezkoder/springjwt/exceptions/BadRequestException.java
@@ -0,0 +1,9 @@
+package com.bezkoder.springjwt.exceptions;
+
+public class BadRequestException extends RuntimeException{
+
+	public BadRequestException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
