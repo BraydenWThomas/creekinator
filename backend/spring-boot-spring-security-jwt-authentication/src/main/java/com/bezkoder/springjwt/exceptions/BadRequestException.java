37b4acb63304cabed60adb01f8ef873c8117f1dc
Yingrui Zhang <yingruizhang@yingruiz-macbook-pro.local>
2023-03-08 12:53:08 +1100

    wrong version, deleted backend

--- backend/spring-boot-spring-security-jwt-authentication/src/main/java/com/bezkoder/springjwt/exceptions/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.bezkoder.springjwt.exceptions;
-
-public class BadRequestException extends RuntimeException{
-
-	public BadRequestException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-}
--- backend/spring-boot-spring-security-jwt-authentication/src/main/java/com/bezkoder/springjwt/exceptions/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.bezkoder.springjwt.exceptions;
-
-public class NotFoundException extends RuntimeException{
-
-	public NotFoundException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-}
