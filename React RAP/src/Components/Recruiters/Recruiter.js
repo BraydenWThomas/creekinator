31eca71be515ee2ac6c85bb8498521c0dad680eb
joseph mao <joseph.mao@fdmgroup.com>
2023-03-13 17:42:57 +1100

    Recruiter.js fix

--- React RAP/src/Components/Recruiters/Recruiter.js
+++ React RAP/src/Components/Recruiters/Recruiter.js
@@ -82,133 +82,69 @@ const Recruiter = () => {
 
             </Router> */}
 
-                <div className='candidatesInfo' style={{ marginTop: "30px" }}>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            '& > :not(style)': {
-                                m: 1,
-                                width: '100%',
-                                height: 300
-                            }
-                        }}
-                    >
-                        {displayState === "Candidate"
-                            ?
-                            <div style={{ clear: "both" }}>
-                                <div className='applicantToolBar' style={{ display: 'flex' }}>
-                                    <Typography component="h2" variant="h4" style={{ flex: 1, margin: 10}}> Applied </Typography>
-                                    <div className='filter'>
+        <div className='candidatesInfo' style={{ marginTop: "30px" }}>
 
-                                        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                                            <InputLabel id="filter"></InputLabel>
-                                            <Select
-                                                labelId="filter"
-                                                id="filter"
-                                            >
-                                                <MenuItem value="Name">Name</MenuItem>
-                                                <MenuItem value="Stream">Stream</MenuItem>
-                                                <MenuItem value="Year of Graduation">Year of Graduation</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                        <button className='candidateAdd' onClick={addCandidateBox}><AddIcon /></button>
-                                    </div>
-                                </div >
-                                {inputList}
-                            </div>
+          {displayState === "Candidate"
+            ?
+            <div style={{ clear: "both" }}>
+              <div className='applicantToolBar'>
+                <h2 style={{ float: "left", paddingBottom: "10px" }}> Applied </h2>
+                <div style={{ float: 'right' }}>
+                  <button className='candidateSort'> <SortByAlphaIcon /> </button>
+                  <select placeholder='filter' id='filterCandidate' style={{ textAlign: 'center', height: "20px", padding: "10px" }}>
+                    <option value="default" disabled> filter </option>
+                    <option value="Name"> Name </option>
+                    <option value="Stream"> Stream </option>
+                    <option value="GradYear"> Year of Graduation </option>
+                  </select>
+                  <a href="/candidate/create" target="_blank">
+                    <button 
+                      className='candidateAdd'>
+                        <AddIcon />
+                    </button>
+                  </a>
+                </div>
+              </div >
+              {candidates.map(candidate => (
+                <div key={candidate.id}>
+                  <CandidateSelectBox candidate={candidate} />
+                </div>
+              ))}
+            </div>
 
+            :
 
+            <div style={{ clear: "both" }}>
+              <div className='assessmentToolBar'>
+                <h2 style={{ float: "left", paddingBottom: "10px" }}>Upcoming</h2>
+                <div style={{ float: 'right' }}>
+                  <button className='candidateSort'><SortByAlphaIcon /></button>
+                  <select id='filterCandidate' style={{ textAlign: 'center', height: "20px", padding: "10px" }}>
+                    <option value="Name">Name</option>
+                    <option value="Stream">Stream</option>
+                    <option value="GradYear">Year of Graduation</option>
+                  </select>
+                  <a href='/ac/create' target="_blank"><button className='candidateAdd'><AddIcon /></button></a>
+                </div>
+              </div>
 
-                            :
+              <AssessmentCentreInfo statustype="upcome" />
 
-                            <div className='candidatesInfo' style={{ marginTop: "30px" }}>
-                                <div className='assessmentToolBar' style={{ display: 'flex' }}>
-                                    <Typography component="h2" variant="h4" style={{ flex: 1, margin: 10 }}> Upcoming </Typography>
-                                    <div className='filter'>
+              <div className="scrollArrows" style={{ float: "right", marginRight: "20px", marginTop: "20px" }}>
+                <button className="leftIcon"><ChevronLeftIcon /></button>
+                <button className="rightIcon"><ChevronRightIcon /></button>
+              </div>
 
-                                        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                                            <InputLabel id="filter"></InputLabel>
-                                            <Select
-                                                labelId="filter"
-                                                id="filter"
-                                            >
-                                                <MenuItem value="Name">Name</MenuItem>
-                                                <MenuItem value="Stream">Stream</MenuItem>
-                                                <MenuItem value="Year of Graduation">Year of Graduation</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                        <a href='/createcandidate' target="_blank"><button className='candidateAdd'><AddIcon/></button></a>
-                                    </div>
-                                </div>
-
-                                <AssessmentCentreInfo statustype="upcomeInterviewer" />
-                                <div className="scrollArrows">
-                                    <button className="leftIcon"><ChevronLeftIcon /></button>
-                                    <button className="rightIcon"><ChevronRightIcon /></button>
-                                </div>
-
-                                <div className='assessmentToolBar' style={{ display: 'flex' }}>
-                                    <Typography component="h2" variant="h4" style={{ flex: 1, margin: 10 }}> Past </Typography>
-                                    <div className='filter'>
-
-                                        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                                            <InputLabel id="filter"></InputLabel>
-                                            <Select
-                                                labelId="filter"
-                                                id="filter"
-                                            >
-                                                <MenuItem value="Name">Name</MenuItem>
-                                                <MenuItem value="Stream">Stream</MenuItem>
-                                                <MenuItem value="Year of Graduation">Year of Graduation</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                        <a href='/createcandidate' target="_blank"><button className='candidateAdd'><AddIcon/></button></a>
-                                    </div>
-                                </div>
-                                <AssessmentCentreInfo statustype="pastInterviewer" />
-                                <div className="scrollArrows">
-                                    <button className="leftIcon"><ChevronLeftIcon /></button>
-                                    <button className="rightIcon"><ChevronRightIcon /></button>
-                                </div>
-
-                            </div>
-
-                            // <div style={{ clear: "both" }}>
-                            //     <div className='assessmentToolBar'>
-                            //         <h2 style={{ float: "left", paddingBottom: "10px" }}>Upcomming</h2>
-
-                            //         <div style={{ float: 'right' }}>
-                            //             <select id='filterCandidate' style={{ textAlign: 'center', height: "20px", padding: "10px" }}>
-                            //                 <option value="Name">Name</option>
-                            //                 <option value="Stream">Stream</option>
-                            //                 <option value="GradYear">Year of Graduation</option>
-                            //             </select>
-                            //             <a href='/createcandidate' target="_blank"><button className='candidateAdd'><AddIcon /></button></a>
-                            //         </div>
-                            //     </div>
-
-                            //     <AssessmentCentreInfo statustype="upcome" />
-                            //     <div className="scrollArrows" style={{ float: "right", marginRight: "20px", marginTop: "20px" }}>
-                            //         <button className="leftIcon"><ChevronLeftIcon /></button>
-                            //         <button className="rightIcon"><ChevronRightIcon /></button>
-                            //     </div>
-
-                            //     <div className='assessmentToolBar' style={{ clear: "both" }}>
-                            //         <h2 style={{ float: "left", marginBottom: "10px" }}>Past</h2>
-                            //         <div style={{ float: 'right' }}>
-                            //             <select placeholder='filter' id='filterCandidate' style={{ textAlign: 'center', height: "20px", padding: "10px" }}>
-                            //                 <option value="default" disabled>filter</option>
-                            //                 <option value="Name">Name</option>
-                            //                 <option value="Stream">Stream</option>
-                            //                 <option value="GradYear">Year of Graduation</option>
-                            //             </select>
-                            //         </div>
-                            //     </div>
-                            //     <AssessmentCentreInfo statustype="past" />
-
-                            // </div>
-                        }
-                    </Box>
+              <div className='assessmentToolBar' style={{ clear: "both" }}>
+                <h2 style={{ float: "left", marginBottom: "10px" }}>Past</h2>
+                <div style={{ float: 'right' }}>
+                  <button className='candidateSort'><SortByAlphaIcon /></button>
+                  <select placeholder='filter' id='filterCandidate' style={{ textAlign: 'center', height: "20px", padding: "10px" }}>
+                    <option value="default" disabled>filter</option>
+                    <option value="Name">Name</option>
+                    <option value="Stream">Stream</option>
+                    <option value="GradYear">Year of Graduation</option>
+                  </select>
                 </div>
               </div>
 
