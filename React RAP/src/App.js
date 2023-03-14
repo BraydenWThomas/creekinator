1cf38c0f215ef26d22c5f31e69290959c06a22a5
Kaiduo Jin <kaiduo.jin@fdmgroup.com>
2023-03-14 11:47:35 +1100

    user authentication mainly in App.js and LoginPage

--- React RAP/src/App.js
+++ React RAP/src/App.js
@@ -1,8 +1,5 @@
-// React + css
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Navigate, useNavigate, Router } from 'react-router-dom';
 import './App.css';
-
-// Componenets
 import AdminDashboard from './Components/AdminDashboard';
 import Recruiter from './Components/Recruiters/Recruiter';
 import Interviewer from './Components/Interviewers/Interviewer';
@@ -13,12 +10,13 @@ import ViewAC from './Components/Interviewers/ViewAC';
 import ViewUpcomingAC from './Components/Recruiters/ViewUpcomingAC';
 import ViewPastAC from './Components/Recruiters/ViewPastAC';
 import CreateAC from './Components/Recruiters/CreateAC';
-import UpdateAC from './Components/Recruiters/UpdateAC';
-import LoginPage from './Components/LoginPage';
-
-// Material UI
+import React, { useState, useEffect } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { blue } from '@mui/material/colors';
+import LoginPage from './Components/LoginPage';
+import handleClick from './Components/LoginPage';
+import NavBar from './Components/NavBar';
+import { Button } from '@mui/material';
 
 const FDMtheme = createTheme({
   palette: {
@@ -29,71 +27,127 @@ const FDMtheme = createTheme({
   },
 });
 
-const App = () => {
-  const router = createBrowserRouter([
-    {
-      path:"/",
-      element: <LoginPage />
-    },
 
-    // Users
-    {
-      path:"/recruiter",
+const App = () => {
+
+
+  const handleClick = (event) => {
+
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    const body =
+      JSON.stringify({
+        "username": data.get('username'),
+        "password": data.get('password')
+      });
+
+    const requestOptions = {
+      headers: { 'content-type': 'application/json' },
+      method: "POST",
+      body: body,
+      redirect: 'follow'
+    };
+
+    fetch(" http://localhost:8080/api/auth/signin", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+
+       
+
+        if (result.username) {
+
+          localStorage.setItem('status', result.roles[0])
+          if (result.roles[0] == "ROLE_RECRUITER") {
+            window.location.href = "/recruiter"
+          }
+          else if (result.roles[0] == "ROLE_INTERVIEWER") {
+            window.location.href = "/interviewer"
+          }
+
+
+        }}) 
+      .catch(error => console.log('error', error));
+
+
+
+
+
+
+  };
+
+  const routes = [{
+    path: "/",
+    element: <LoginPage onClick={handleClick} />
+  }]
+
+  if (localStorage.getItem('status') == "ROLE_RECRUITER") {
+    routes.push({
+      path: "/recruiter",
       element: <Recruiter />
     },
-    {
-      path:"/interviewer",
+      {
+        path: "/candidate/create",
+        element: <CreateCandidate />
+      },
+      {
+        path: "/candidate/update/:abc",
+        element: <UpdateCandidate />
+      },
+      {
+        path: "/candidate/info/:abc",
+        element: <CandidateInformation />
+      },
+      {
+        path: "/viewac/:abc",
+        element: <ViewAC />
+      },
+      {
+        path: "/viewupcomingac/:abc",
+        element: <ViewUpcomingAC />
+      }
+      ,
+      {
+        path: "/viewpastac/:abc",
+        element: <ViewPastAC />
+      }
+      ,
+      {
+        path: "/createac",
+        element: <CreateAC />
+      }
+    )
+  }
+
+  if (localStorage.getItem('status') == "ROLE_INTERVIEWER") {
+    routes.push({
+      path: "/interviewer",
       element: <Interviewer />
     },
-    {
-      path:"/admin",
-      element: <AdminDashboard />
-    },
+      {
+        path: "/candidateinformation/:abc",
+        element: <CandidateInformation />
+      },
+      {
+        path: "/viewac/:abc",
+        element: <ViewAC />
+      })
+  }
 
-    // User webpages
+  const routerPage = createBrowserRouter(routes);
 
-    // Candidates
-    {
-      path:"/candidate/create",
-      element: <CreateCandidate />
-    },
-    {
-      // path:"/candidate/info/name=:candidateName&id=:candidateId",
-      path: "/candidate/info/:candidateId",
-      element: <CandidateInformation />
-    },
-    {
-      path:"/candidate/update/:candidateId",
-      element: <UpdateCandidate />
-    },
+  const logout = () => {
 
-    // AC Info
-    {
-      path:"/ac/view/:abc",
-      element: <ViewAC />
-    },
-    {
-      path:"/ac/view-upcoming/:abc",
-      element: <ViewUpcomingAC />
-    },
-    {
-      path:"/ac/view-past/:abc",
-      element: <ViewPastAC />
-    },
-    {
-      path:"/ac/create",
-      element: <CreateAC />
-    },
-    {
-      path:"/ac/update",
-      element: <UpdateAC />
-    }
-    
-  ]);
+    localStorage.removeItem('status');
+    window.location.href = "/"
+
+  }
 
   return (
+
     <ThemeProvider theme={FDMtheme}>
-    <RouterProvider router = {router} />
+      <Button onClick={logout}>LOGOUT</Button>
+      <RouterProvider router={routerPage} />
     </ThemeProvider>
   )
 
