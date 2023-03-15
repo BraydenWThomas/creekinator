37b14eb9ee1213d88b02b9ebfb68f17f87fd9642
joseph mao <joseph.mao@fdmgroup.com>
2023-03-15 23:27:50 +1100

    Fixed admin creating users -> Sales and Tech interviewers can now be posted to database as an interviewer user
    Added in ac info db data to interviewers page -> currently shows ALL ac's -> chevron arrows dont work anymore
    
    Fixed url links to reflect which user type is viewing webpages

--- React RAP/src/Components/Recruiters/AssessmentCentreInfo.js
+++ React RAP/src/Components/Recruiters/AssessmentCentreInfo.js
@@ -198,9 +198,9 @@ const AssessmentCentreInfo = ({ statustype, ac }) => {
             <MenuList statustype={statustype} /> :
             (statustype === "pastAC" ?
               <MenuList statustype={statustype} /> :
-              (statustype === "interviewer" ?
+              (statustype === "interviewerAC" ?
                 <MenuList statustype={statustype} /> :
-                <button className='acDetails'>....</button>
+                <></>
               )
             )
           }
